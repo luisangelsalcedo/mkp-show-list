ba6ac6491009a96337d9491123afdcddb5fd53d0
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2021-12-27 14:52:21 -0500

    add Mostrando una lista

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-import React, { Component } from 'react';
+import React from "react";
 
 /// Mostrar una lista en base a un arreglo
+const App = () => {
+  const list = ["Javascript", "CSS", "HTLM", "React"];
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      list: ['Javascript', 'CSS', 'HTLM', 'React']
-    }
-  }
-  render() {
-    return (
-      <div style={styles.container}>
-        <h1 style={styles.title}>Las tecnologias del Front</h1>
-        <ul style={styles.list}>
-          {/* Tu codigo va aca */}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>Las tecnologias del Front</h1>
+      <ul style={styles.list}>
+        {list.map((content, i) => (
+          <li key={String(i)}>{content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const styles = {
   container: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    marginTop: '100px'
+    marginTop: "100px",
   },
   list: {
-    fontSize: '20px',
-    flexGrow: 6
-  }
-}
+    fontSize: "20px",
+    flexGrow: 6,
+  },
+};
 
 export default App;
